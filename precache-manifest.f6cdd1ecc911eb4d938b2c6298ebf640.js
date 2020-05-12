0508f5f98fa882298f17b33e199f44945e36b4e5
SashaMargaryan <sasha.margaryan@inbox.ru>
2020-05-12 18:43:52 +0400

    Updates

---
+++ precache-manifest.f6cdd1ecc911eb4d938b2c6298ebf640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bed957d3117fc529e1a319775f42571",
+    "url": "/react-city/index.html"
+  },
+  {
+    "revision": "e7418f15891fb88f8434",
+    "url": "/react-city/static/css/main.24049375.chunk.css"
+  },
+  {
+    "revision": "b4440c85904c68272732",
+    "url": "/react-city/static/js/2.dacbd8b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-city/static/js/2.dacbd8b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7418f15891fb88f8434",
+    "url": "/react-city/static/js/main.926cea56.chunk.js"
+  },
+  {
+    "revision": "5bd5c34b38996637f4da",
+    "url": "/react-city/static/js/runtime-main.98c12061.js"
+  }
+]);
